e8650b8cb0a769b108bed63146c6d6e6c89de32a
Jens Sabel <jens.sabel@abo.fi>
2023-09-20 16:09:07 +0300

    Exercise 1.7 Completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,20 +8,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  const HandlePositive = () => setGood(good+1)
+  const HandleNeutral = () => setNeutral(neutral+1)
+  const HandleBad = () => setBad(bad+1)
+  const total = good + neutral + bad
+  const average = (good - bad)/total
+  const positive = good/total * 100 + "%"
 
   return (
     <div>
       <div>
         <Header text="Give Feedback"/>
-        <Button handleClick={() => setGood(good + 1)} text="Good"/>
-        <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
-        <Button handleClick={() => setBad(bad + 1)} text="Bad"/>
+        <Button handleClick={HandlePositive} text="Good"/>
+        <Button handleClick={HandleNeutral} text="Neutral"/>
+        <Button handleClick={HandleBad} text="Bad"/>
       </div>
       <div>
         <Header text="Statistics"/>
         <Statistics text="Good" score={good}/>
         <Statistics text="Neutral" score={neutral}/>
         <Statistics text="Bad" score={bad}/>
+        <Statistics text="All" score={total}/>
+        <Statistics text="Average" score={average}/>
+        <Statistics text="Positive" score={positive}/>
       </div>
     </div>
   )
