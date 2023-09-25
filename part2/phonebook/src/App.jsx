88cf3031c546de5ef26e903f69af2e60eea8d8bd
Jens Sabel <jens.sabel@abo.fi>
2023-09-25 14:30:36 +0300

    Exercise 2.9 Completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
 
-const Person = (props) => <p>{props.name}</p>
+const Person = (props) => <p>{props.name} {props.number}</p>
+
+let ShowPeople = ({persons}) => {
+  return(
+    <div>
+      {persons.map(persons =>
+      <Person key={persons.id} name={persons.name} number={persons.number}/>
+    )}
+    </div>
+  )
+}
 
 const App = () => { 
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas' }
-  ]) 
+    { id: 1, name: 'Arto Hellas', number: '045-3128965' },
+    { id: 2, name: 'Dudley Jordan', number: '021-431941'},
+    { id: 3, name: 'Lynette Acosta', number: '056-123041'},
+    { id: 4, name: 'Eddie Mullins', number: '123-513123'}
+  ])
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState(persons)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -14,19 +30,27 @@ const App = () => {
 
     const nameObject = {
       id: persons.length + 1,
-      name: newName
+      name: newName,
+      number: newNumber
     }
     
     let i
     for (i = 0; i < persons.length; i++) {
       if (persons[i].name === nameObject.name) {
         alert(`${newName} is already added to phonebook`)
-        return(setNewName('')) //Removes the already existing name from input-field
+        return(
+          setNewName(''),
+          setNewNumber('')
+          ) 
       } 
     } 
-
+    //If person is not found already the person gets added
     setPersons(persons.concat(nameObject))
+    //To update the temp array used to show contacts on-page
+    setFilter(filter.concat(nameObject))
+    //Cleanup 
     setNewName('')
+    setNewNumber('')
     
   }
 
@@ -35,9 +59,30 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleAddNumber = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilter = (event) => {
+    console.log(event.target.value)
+    const query = event.target.value;
+    let updatedList = [...persons];
+    updatedList = updatedList.filter((name) => {
+      return name.name.indexOf(query) !== -1;
+    });
+    setFilter(updatedList)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with: <input
+          onChange={handleFilter}
+        />
+      </div>
+      <h2>Add a new contact</h2>
       <form onSubmit={addPerson}>
         <div>
           Name: <input 
@@ -46,15 +91,17 @@ const App = () => {
             />
         </div>
         <div>
-          <button type="submit">add</button>
+          Number: <input 
+            value={newNumber}
+            onChange={handleAddNumber}
+          />
+        </div>
+        <div>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map(person =>
-            <Person key={person.id} name={person.name}/>
-          )}
-      </div>
+      <ShowPeople persons={filter}/>
     </div>
   )
 }
