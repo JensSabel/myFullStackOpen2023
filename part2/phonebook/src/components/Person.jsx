bf785a6655948dd2dad8984591c0bf56e5af3f18
Jens Sabel <jens.sabel@abo.fi>
2024-01-11 13:34:19 +0200

    Exercise 2.14 Completed

--- part2/phonebook/src/components/Person.jsx
+++ part2/phonebook/src/components/Person.jsx
@@ -1,3 +1,13 @@
-const Person = (props) => <p>{props.name} {props.number}</p>
+import personService from '../services/ServicePerson'
+
+const handleDelete = ({name, id}) => {
+    if (window.confirm(`Do you want to delete ${name} from the phonebook?`)) {
+        personService.deletePerson(id).then(() => {
+            window.location.reload();
+        })
+    }
+}
+
+const Person = (props) => <p>{props.name} {props.number} <button onClick={() => handleDelete({name: props.name, id: props.id})} type="submit">Remove</button></p>
 
 export default Person
--- part2/phonebook/src/services/ServicePerson.js
+++ part2/phonebook/src/services/ServicePerson.js
@@ -11,4 +11,9 @@ const create = newObject => {
     return request.then(response => response.data)
 }
 
-export default {getAll, create}
+const deletePerson = id => {
+    const request = axios.delete(`http://localhost:3001/persons/${id}`)
+    return request.then(response => response.data)
+}
+
+export default {getAll, create, deletePerson}
